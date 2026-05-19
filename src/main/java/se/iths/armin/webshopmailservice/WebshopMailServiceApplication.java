4bb26190f6169780a0da46e3a820b4a42a8898cb
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-05-19 13:07:49 +0200

    add gh actions

---
+++ src/main/java/se/iths/armin/webshopmailservice/WebshopMailServiceApplication.java
@@ -0,0 +1,13 @@
+package se.iths.armin.webshopmailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopMailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopMailServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/armin/webshopmailservice/WebshopMailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.armin.webshopmailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopMailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
