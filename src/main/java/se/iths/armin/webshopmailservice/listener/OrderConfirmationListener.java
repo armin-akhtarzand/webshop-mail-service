87b4da13abc5fb11d670d1f1507bcd50b394da67
anastevanovic <Ana.Stevanovic@iths.se>
2026-05-27 06:07:36 +0200

    updatering pom.xl och mail config.

--- src/main/java/se/iths/armin/webshopmailservice/listener/OrderConfirmationListener.java
+++ src/main/java/se/iths/armin/webshopmailservice/listener/OrderConfirmationListener.java
@@ -5,18 +5,18 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 import se.iths.armin.webshopmailservice.config.RabbitMQConfig;
 import se.iths.armin.webshopmailservice.message.OrderConfirmationMessage;
-import se.iths.armin.webshopmailservice.service.MailService;
+import se.iths.armin.webshopmailservice.service.OrderConfirmationService;
 
 @Component
 public class OrderConfirmationListener {
-    private final MailService mailService;
+    private final OrderConfirmationService orderConfirmationService;
 
-    public OrderConfirmationListener(MailService mailService) {
-        this.mailService = mailService;
+    public OrderConfirmationListener(OrderConfirmationService orderConfirmationService) {
+        this.orderConfirmationService = orderConfirmationService;
     }
 
     @RabbitListener(queues = RabbitMQConfig.ORDER_CONFIRMATION_QUEUE)
     public void receiveOrderConfirmation(OrderConfirmationMessage message) {
-        mailService.sendOrderConfirmation(message);
+        orderConfirmationService.sendOrderConfirmation(message);
     }
 }
